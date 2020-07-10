341bd75baa7c0d3e0db56e6b8f2b01bda825b155
Andy Adams <andrewdcadams@gmail.com>
2020-07-10 15:21:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,19 @@
 # Add  code here!
 
 def prime?(num)
-  puts num
+
   num = num.abs
-  puts num 
+
    n = 2
   return false if num == 1 || num == 0 
   while n < (num)
-  puts(num % n)
     if num % n == 0
-      puts("false")
+
       return false
      end
      n += 1
   end
-  puts("prime")
+
   true
 end
 
