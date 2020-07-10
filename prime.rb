db013e2061ba4e5daaff9d94cd8e379ccfe2b7b4
Andy Adams <andrewdcadams@gmail.com>
2020-07-10 15:05:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+
+def prime?(num)
+  puts num
+  num = num.abs
+  puts num 
+   n = 2
+  while n < (num/1.5)
+  puts(num % n)
+    if num % n == 0
+      puts("false")
+      return false
+       n += 1
+     end
+  end
+  puts("prime")
+  true
+end
+
+
+prime?(-9)
